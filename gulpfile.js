d52959a0cae5f8bc2e4490d4204c3ff1a732aef0
Arum <arum.galadima@gmail.com>
2017-12-18 02:06:57 -0500

    second to last
    almost done

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,36 @@
 var gulp = require('gulp'),
 	concat = require('gulp-concat'),
 	uglify = require('gulp-uglify'),
-	rename = require('gulp-rename');
+	rename = require('gulp-rename'),
+	sass = require('gulp-sass'),
+	maps = require('gulp-sourcemaps'),
+	minCss = require('gulp-clean-css');
 
-gulp.task('concatScripts', function() {
-	gulp.src([
+//concat and minify js
+gulp.task('scripts', function() {
+	return gulp.src([
 		'js/circle/autogrow.js',
 		'js/circle/circle.js'
 	])
-	.pipe(concat('app.js'))
-	.pipe(gulp.dest('js'));
+	.pipe(maps.init())
+	.pipe(concat('global.js'))
+	.pipe(uglify())
+	.pipe(rename('all.min.js'))
+	.pipe(maps.write('./'))
+	.pipe(gulp.dest('dist/scripts'));
+});
+
+//concat and minify css
+gulp.task('compileSass', function() {
+	gulp.src('sass/global.scss')
+		.pipe(maps.init())
+		.pipe(sass())
+		.pipe(minCss())
+		.pipe(rename('all.min.css'))
+		.pipe(maps.write('./'))
+		.pipe(gulp.dest('dist/styles'));
+});
+
+gulp.task('default', ['scripts'], function() {
+	console.log('Running the default task');
 });
